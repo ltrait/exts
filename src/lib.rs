a8490a6fcd7b5c906cd342bf1ba2a52b31420db6
satler <satler@satler.dev>
2025-03-04 14:50:10 +0900

    feat: impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ltrait::{source::Source, tokio_stream};
+
+pub mod icon;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DesktopError {
+    // Stringは名前
+    #[error("Failed to find icon of {0}")]
+    NoIcon(String),
+    #[error("Failed to open file: {0}")]
+    OpenFile(#[source] std::io::Error),
+    #[error("Failed to decode the image: {0}")]
+    ImageDecode(#[source] image::ImageError),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct DesktopEntry {
+    pub entry: freedesktop_desktop_entry::DesktopEntry,
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    /// base64 encoded icon of the entry
+    pub icon: Option<String>,
+}
+
+// 楽をするためにfreedesktop_desktop_entryを使っているからStreamではなく、性能を最大限に活かしきれていない
+pub fn new<'a>() -> Result<Source<'a, DesktopEntry>, DesktopError> {
+    use freedesktop_desktop_entry::{Iter, default_paths};
+    let entries = Iter::new(default_paths()).entries::<String>(None).map(|e| {
+        let icon = e.icon().and_then(|n| icon::lookup(n).ok());
+        DesktopEntry { entry: e, icon }
+    });
+
+    Ok(Box::pin(tokio_stream::iter(entries)))
 }
