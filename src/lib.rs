9d1543b5194bbf57e9e08ab571e6042257ea5527
satler <satler@satler.dev>
2025-03-08 13:54:27 +0900

    feat: impl todoes

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,14 @@
 //! See also [User:Jesse/NewFrecency on mozilla wiki](https://wiki.mozilla.org/User:Jesse/NewFrecency?title=User:Jesse/NewFrecency)
 //!
 //! Create a database on `<XDG_DATA_HOME>/ltrait/frency/frency.sqlite`
+use std::collections::HashMap;
 use std::path::PathBuf;
-use std::time::{Duration, Instant};
+use std::time::{Duration, SystemTime};
 
-use ltrait::color_eyre::eyre::{OptionExt, Result};
+use chrono::{DateTime, TimeDelta, Utc};
+use ltrait::color_eyre::eyre::{OptionExt, Result, WrapErr};
+use ltrait::{Action, Sorter};
+use rusqlite::{Connection, params};
 
 /// The context of ltrait-sorter-frency
 /// `ident` must be unique.
@@ -20,15 +24,15 @@ pub struct Context<'a> {
 /// * `samples_count` pick up numbers that used to caliculate the score
 pub struct FrencyConfig {
     half_life: Duration,
+    type_ident: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Entry {
     ident: String,
-
     pub(crate) score: f64,
 
-    date: Instant,
+    date: DateTime<Utc>,
 }
 
 impl Entry {
@@ -36,19 +40,19 @@ impl Entry {
         Self {
             ident,
             score: 0.,
-            date: Instant::now(),
+            date: Utc::now(),
         }
     }
 
     fn update<'a>(mut self, ctx: &Context<'a>, config: &FrencyConfig) -> Self {
         let ln2 = (2f64).ln();
-        let now = Instant::now();
-        let diff = now.duration_since(self.date);
+        let now = Utc::now();
+        let diff = now.signed_duration_since(self.date);
 
         self.score = {
             self.score
-                * (-(ln2 / (config.half_life.as_secs_f64() / (60f64 * 60f64))) * diff.as_secs_f64()
-                    / (60f64 * 60f64))
+                * (-(ln2 / (config.half_life.as_secs_f64() / 3600f64)) // as hour_f64
+                    * (diff.num_minutes() as f64 / (60f64)))
                     .exp()
                 + ctx.bonus
         };
@@ -58,6 +62,119 @@ impl Entry {
     }
 }
 
-fn db_dir() -> Option<PathBuf> {
-    dirs::data_dir().and_then(|p| Some(p.join("ltrait/frency/frency.sqlite")))
+pub struct Frency {
+    entries: HashMap<String, Entry>,
+    config: FrencyConfig,
+}
+
+impl Frency {
+    pub fn new(config: FrencyConfig) -> Result<Self> {
+        Ok(Self {
+            entries: {
+                let conn = new_conn()?;
+                // type_ident
+                let mut stmt = conn
+                    .prepare(
+                        "SELECT ident, score, date FROM frecency_entries where type_ident = ?1",
+                    )
+                    .unwrap();
+
+                let entries = stmt
+                    .query_map([&config.type_ident], |row| {
+                        Ok(Entry {
+                            ident: row.get(0).unwrap(),
+                            score: row.get(1).unwrap(),
+                            date: row.get(2).unwrap(),
+                        })
+                    })
+                    .unwrap();
+
+                let mut v = HashMap::new();
+
+                for ei in entries {
+                    let ei: Entry = ei.unwrap();
+                    v.insert(ei.ident.clone(), ei);
+                }
+
+                v
+            },
+            config,
+        })
+    }
+}
+
+impl<'a> Sorter<'a> for Frency {
+    type Context = Context<'a>;
+
+    fn compare(&self, lhs: &Self::Context, rhs: &Self::Context, _: &str) -> std::cmp::Ordering {
+        ((self.entries.get(lhs.ident))
+            .and_then(|e| Some(e.score))
+            .unwrap_or(0.))
+        .partial_cmp(
+            &(self.entries.get(rhs.ident))
+                .and_then(|e| Some(e.score))
+                .unwrap_or(0.),
+        )
+        .unwrap_or(std::cmp::Ordering::Equal)
+    }
+}
+
+impl<'a> Action<'a> for Frency {
+    type Context = Context<'a>;
+
+    fn act(&self, ctx: &Self::Context) -> Result<()> {
+        // self.entries
+        let new_entry = if self.entries.contains_key(ctx.ident) {
+            self.entries
+                .get(ctx.ident)
+                .unwrap()
+                .clone()
+                .update(ctx, &self.config)
+        } else {
+            Entry::new(ctx.ident.into())
+        };
+
+        let conn = new_conn()?;
+
+        conn.execute(
+            "INSERT INTO frecency_entries (type_ident, ident, score, date)
+            VALUES (?1, ?2, ?3, ?4)
+            ON CONFLICT(type_ident, ident) DO UPDATE SET
+              score = EXCLUDED.score,
+              date = EXCLUDED.date;",
+            params![
+                &self.config.type_ident,
+                &new_entry.ident,
+                new_entry.score,
+                new_entry.date,
+            ],
+        )
+        .wrap_err("Failed to insert or update")?;
+
+        Ok(())
+    }
+}
+
+fn new_conn() -> Result<Connection> {
+    fn db_dir() -> Option<PathBuf> {
+        dirs::data_dir().and_then(|p| Some(p.join("ltrait/frency/frency.sqlite")))
+    }
+
+    let conn = Connection::open(db_dir().ok_or_eyre("Failed to get the path to store db")?)
+        .wrap_err("Failed to open a connectioin")?;
+
+    conn.execute(
+        r"CREATE TABLE IF NOT EXISTS frecency_entries (
+            id INTEGER PRIMARY KEY,
+            type_ident TEXT NOT NULL,
+            ident TEXT NOT NULL,
+            score REAL NOT NULL,
+            date TEXT NOT NULL,
+            UNIQUE(type_ident, ident)
+        );",
+        [],
+    )
+    .wrap_err("Failed to create new table")?;
+
+    Ok(conn)
 }
