f6a37865b2c2b2571f4cf1e28b2ef715320324a0
satler <satler@satler.dev>
2025-03-21 17:45:39 +0900

    feat: lazy icon

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,20 @@ pub enum DesktopError {
 
 pub struct DesktopEntry {
     pub entry: freedesktop_desktop_entry::DesktopEntry,
+}
 
-    /// base64 encoded icon of the entry
-    pub icon: Option<String>,
+impl DesktopEntry {
+    pub fn icon(&self) -> Option<String> {
+        self.entry.icon().and_then(|n| icon::lookup(n).ok())
+    }
 }
 
 // 楽をするためにfreedesktop_desktop_entryを使っているからStreamではなく、性能を最大限に活かしきれていない
 pub fn new<'a>() -> Result<Source<'a, DesktopEntry>, DesktopError> {
     use freedesktop_desktop_entry::{Iter, default_paths};
-    let entries = Iter::new(default_paths()).entries::<String>(None).map(|e| {
-        let icon = e.icon().and_then(|n| icon::lookup(n).ok());
-        DesktopEntry { entry: e, icon }
-    });
+    let entries = Iter::new(default_paths())
+        .entries::<String>(None)
+        .map(|e| DesktopEntry { entry: e });
 
     Ok(Box::pin(tokio_stream::iter(entries)))
 }
