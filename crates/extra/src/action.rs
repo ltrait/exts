e8f03ad1697b784e6d3d98114435b3a55f000948
satler <satler@satler.dev>
2025-11-02 16:12:43 +0900

    feat(extra): add ActionIf

---
+++ crates/extra/src/action.rs
@@ -0,0 +1,62 @@
+use ltrait::{Action, action::ActionWrapper};
+
+pub trait ActionExt: Action
+where
+    <Self as Action>::Context: Sync + Send,
+{
+    fn to_if<Cushion, F, TransF>(self, f: F, transformer: TransF) -> impl Action<Context = Cushion>
+    where
+        Self: Sized,
+        Cushion: Sync + Send,
+        F: Fn(&Cushion) -> bool + Send,
+        TransF: Fn(&Cushion) -> <Self as Action>::Context + Send,
+    {
+        ActionIf::new(self, f, transformer)
+    }
+}
+
+pub struct ActionIf<T, Ctx, F>
+where
+    T: Action<Context = Ctx>,
+    F: Fn(&Ctx) -> bool + Send,
+    Ctx: Sync,
+{
+    inner: T,
+
+    f: F,
+}
+
+impl<Cushion, F, Inner, TransF, Ctx>
+    ActionIf<ActionWrapper<Ctx, Inner, TransF, Cushion>, Cushion, F>
+where
+    F: Fn(&Cushion) -> bool + Send,
+    Cushion: Sync + Send,
+    TransF: Fn(&Cushion) -> Ctx + Send,
+    Inner: Action<Context = Ctx>,
+    Ctx: Sync + Send,
+{
+    pub fn new(inner: Inner, f: F, transformer: TransF) -> Self {
+        Self {
+            inner: ActionWrapper::new(inner, transformer),
+            f,
+            // _ctx: PhantomData,
+        }
+    }
+}
+
+impl<T, Ctx, F> Action for ActionIf<T, Ctx, F>
+where
+    T: Action<Context = Ctx>,
+    F: Fn(&Ctx) -> bool + Send,
+    Ctx: Sync + Send,
+{
+    type Context = Ctx;
+
+    fn act(&self, ctx: &Self::Context) -> ltrait::color_eyre::eyre::Result<()> {
+        if (self.f)(ctx) {
+            self.inner.act(ctx)
+        } else {
+            Ok(())
+        }
+    }
+}
--- crates/extra/src/lib.rs
+++ crates/extra/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod action;
 pub mod filter;
 pub mod scorer;
 pub mod sorter;
